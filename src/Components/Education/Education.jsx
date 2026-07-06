8985a299c4c4b73611b133d9a1d995d59c7a248b
DOLA-DOLA-AKTER <dola.akter20320@gmail.com>
2026-07-06 23:28:00 +0600

    Responsive

--- src/Components/Education/Education.jsx
+++ src/Components/Education/Education.jsx
@@ -26,18 +26,21 @@ export const Education = () => {
                 breakpoint: 992,
                 settings: {
                     slidesToShow: 2,
+                    slidesToScroll: 1,
                 }
             },
             {
                 breakpoint: 768,
                 settings: {
                     slidesToShow: 1,
+                    slidesToScroll: 1,
                 }
             },
             {
                 breakpoint: 570,
                 settings: {
                     slidesToShow: 1,
+                    slidesToScroll: 1,
                 }
             },
 
